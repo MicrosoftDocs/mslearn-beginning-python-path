adea308489803717de2f75969cf1dfe423f248f5
Kristine Toliver (Aquent LLC) <v-krmaxw@microsoft.com>
2021-12-06 14:50:49 -0800

    set kernel per CG

--- modules/create-manage-projects/calc.py
+++
@@ -1,4 +0,0 @@
-print("calculator program")
-first_number = input("first number: ")
-second_number = input("second number: ")
-print(int(first_number) + int(second_number))
--- modules/create-manage-projects/convert.py
+++
@@ -1,4 +0,0 @@
-parsec = 11
-lightyears = 3.26156 * parsec
-
-print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
